09073230add552a30cd18e7fcd22cf64acc338d8
Likhita12-creator <likhitasomarouthu@gmail.com>
2025-09-05 17:03:16 +0530

    modified:   app/data.json 	modified:   app/layout.js 	modified:   components/VantaBackground.js

--- components/VantaBackground.js
+++ components/VantaBackground.js
@@ -1,33 +1,35 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
-import WAVES from "vanta/dist/vanta.waves.min";
 
 export default function VantaBackground() {
   const vantaRef = useRef(null);
   const [vantaEffect, setVantaEffect] = useState(null);
 
   useEffect(() => {
-    if (!vantaEffect && vantaRef.current) {
-      setVantaEffect(
-        WAVES({
-          el: vantaRef.current,
-          THREE,
-          mouseControls: true,
-          touchControls: true,
-          gyroControls: true,
-          minHeight: 200.00,
-          minWidth: 200.00,
-          scale: 1.00,
-          scaleMobile: 1.00,
-          color: 0x0,
-          shininess: 92.00,
-          waveHeight: 25.50,
-          waveSpeed: 0.60,
-          zoom: 0.98
-        })
-      );
+    let BIRDS;
+    // Only run on client
+    if (typeof window !== "undefined") {
+      import("vanta/dist/vanta.birds.min").then((module) => {
+        BIRDS = module.default;
+        if (!vantaEffect && vantaRef.current) {
+          setVantaEffect(
+            BIRDS({
+              el: vantaRef.current,
+              THREE,
+              mouseControls: true,
+              touchControls: true,
+              gyroControls: false,
+              minHeight: 200.0,
+              minWidth: 200.0,
+              scale: 1.0,
+              scaleMobile: 1.0,
+            })
+          );
+        }
+      });
     }
+
     return () => {
       if (vantaEffect) vantaEffect.destroy();
     };
